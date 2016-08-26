0a3ca5cd05d9f196b7fbc46ddb76367f983fe630
Nick Felicelli <nick.felicelli@gmail.com>
2016-08-25 21:17:48 -0400

    First workings of an api

---
+++ lib/pickems/endpoint.ex
@@ -0,0 +1,41 @@
+defmodule Pickems.Endpoint do
+  use Phoenix.Endpoint, otp_app: :pickems
+
+  socket "/socket", Pickems.UserSocket
+
+  # Serve at "/" the static files from "priv/static" directory.
+  #
+  # You should set gzip to true if you are running phoenix.digest
+  # when deploying your static files in production.
+  plug Plug.Static,
+    at: "/", from: :pickems, gzip: false,
+    only: ~w(css fonts images js favicon.ico robots.txt)
+
+  # Code reloading can be explicitly enabled under the
+  # :code_reloader configuration of your endpoint.
+  if code_reloading? do
+    plug Phoenix.CodeReloader
+  end
+
+  plug Plug.RequestId
+  plug Plug.Logger
+
+  plug Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Poison
+
+  plug Plug.MethodOverride
+  plug Plug.Head
+  plug CORSPlug
+
+  # The session will be stored in the cookie and signed,
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt if you would also like to encrypt it.
+  plug Plug.Session,
+    store: :cookie,
+    key: "_pickems_key",
+    signing_salt: "2snDkf26"
+
+  plug Pickems.Router
+end
